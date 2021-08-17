12cc783fcbd0128528fd73f6fcf469d0c73d44b7
yuhuakukude <15577334054@163.com>
2021-08-17 11:08:37 +0800

    Improve BSC FACTORY address

--- src/constants.ts
+++ src/constants.ts
@@ -21,12 +21,12 @@ export enum Rounding {
 
 export const FACTORY_ADDRESS  : {[chainId in ChainId] : string} = {
   [ChainId.MAINNET] : '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
-  [ChainId.BSC] : '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+  [ChainId.BSC] : '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
 }
 
 export const INIT_CODE_HASH : {[chainId in ChainId] : string} = {
   [ChainId.MAINNET] : '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
-  [ChainId.BSC] : '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+  [ChainId.BSC] : '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
 }
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
